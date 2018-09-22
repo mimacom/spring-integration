79b5ecae01f1772abf04aa6d55706258fc79002d
Patrick Dobler <patrick.dobler@gmail.com>
2018-09-22 20:02:56 +0200

    use AutoConfigureAfter with names in order to make sure the ZookeeperLeaderAutoConfiguration.java is invoked after the optional other zookeeper-auto-configurations

--- spring-integration-leader-starter/src/main/java/com/mimacom/spring/integration/leader/providers/zookeeper/ZookeeperLeaderAutoConfiguration.java
+++ spring-integration-leader-starter/src/main/java/com/mimacom/spring/integration/leader/providers/zookeeper/ZookeeperLeaderAutoConfiguration.java
@@ -4,19 +4,21 @@ import com.mimacom.spring.integration.leader.LeaderConfigurationProperties;
 import com.mimacom.spring.integration.leader.providers.LeaderProvider;
 import org.apache.curator.framework.CuratorFramework;
 
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.integration.zookeeper.config.LeaderInitiatorFactoryBean;
 import org.springframework.integration.zookeeper.leader.LeaderInitiator;
 
 @Configuration
-@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
+@AutoConfigureAfter(name = {
+        "org.springframework.cloud.zookeeper.ZookeeperAutoConfiguration",
+        "com.mimacom.spring.integration.zookeeper.ZookeeperAutoConfiguration"
+})
 @ConditionalOnBean(CuratorFramework.class)
 @EnableConfigurationProperties({ZookeeperLeaderConfigurationProperties.class, LeaderConfigurationProperties.class})
 @ConditionalOnProperty(value = "spring-integration.leader.zookeeper.enabled", matchIfMissing = true)
